041fd7b2be144460e97609970e062758b5c553bc
Karanjeet-kumar <karanrajput8400@gmail.com>
2025-01-13 00:54:58 +0530

    Implementing-tailwind-component

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const Logo = () => {
   return (
     <div className="text-3xl text-center py-4">
-      BlogStandard
+      BlogPost-AI
       <FontAwesomeIcon
         icon={faBrain}
         className="pl-2 text-2xl text-slate-400"
