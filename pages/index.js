ac945178eea878bdddbed4908353c09d3ebb1ff6
Karanjeet-kumar <karanrajput8400@gmail.com>
2025-01-13 16:54:12 +0530

    Implementing Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Logo } from "../components/Logo/Logo";
+import HeroImage from "../public/hero.webp";
+
 export default function Home() {
   return (
-    <div>
-      <h1>This is the homepage.</h1>
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
+      <Image src={HeroImage} alt="Hero" fill className="absolute" />
+      <div className="relative z-1 text-white px-5 pb-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm">
+        <Logo />
+        <p>
+          The AI-powered SAAS solution to generate SEO-optimized blog posts in
+          minutes. Get high-quality content, without sacrificing your time.
+        </p>
+        <Link href="/post/new" className="btn mt-2">
+          Begin
+        </Link>
+      </div>
     </div>
   );
 }
